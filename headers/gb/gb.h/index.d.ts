a5e4bf261dbfef3d2040086c2e49486eceb13ec3
Angélo LIMA <angelomiguellima@gmail.com>
2020-12-09 07:56:45 +0100

    feat: add gb/gb.h headers

--- headers/gb/gb.h/index.d.ts
+++ headers/gb/gb.h/index.d.ts
@@ -1,4 +1,19 @@
-declare module 'headers/gb/gb.h' {
+declare module 'gb/gb.h' {
+
+    type Char = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k'
+        | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x'
+        | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K'
+        | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X'
+        | 'Y' | 'Z' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+
     const J_START: number;
+    const J_SELECT: number;
+    const J_A: number;
+    const J_B: number;
+    const J_UP: number;
+    const J_DOWN: number;
+    const J_LEFT: number;
+    const J_RIGHT: number;
+    type UNINT8 = number | Char;
     function waitpad(button: number): void;
 }
